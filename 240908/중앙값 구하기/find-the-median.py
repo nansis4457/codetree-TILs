d22ed3c1c6d45964383abd1c6aef6982d827f79b
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-08 00:11:46 +0900

    Title: 중앙값 구하기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/중앙값 구하기/find-the-median.py
+++ 240908/중앙값 구하기/find-the-median.py
@@ -1,14 +1,17 @@
-a,b,c = map(int,input().split())
-list = []
-list.append(a,b,c)
-list.sort()
-n = len(list)
+a, b, c = map(int, input().split())
+numbers = []  # Avoid using 'list' as a variable name
+numbers.extend([a, b, c])  # Use extend to add multiple elements
+numbers.sort()  # Sort the list
+
+n = len(numbers)
 
 if n % 2 == 1:
-    median = list[n // 2]
+    # If odd, pick the middle element
+    median = numbers[n // 2]
     print(median)
 else:
-    middle1 = list[n // 2 - 1]
-    middle2 = list[n // 2]
+    # If even, calculate the average of the two middle elements
+    middle1 = numbers[n // 2 - 1]
+    middle2 = numbers[n // 2]
     median = (middle1 + middle2) / 2
     print(median)

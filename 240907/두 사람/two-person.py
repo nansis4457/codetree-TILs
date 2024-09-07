8de0433c6e857dc6f10bd30c74b36c7ef93661e5
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-07 23:19:22 +0900

    Title: 두 사람, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240907/두 사람/two-person.py
+++ 240907/두 사람/two-person.py
@@ -3,7 +3,7 @@ b_1,b_2 = input().split()
 
 a_1,b_1 = int(a_1),int(b_1)
 
-if (a_1 >= 19 or b_1 >= 19) and (a_2 == 'M' or b_2 == 'M'):
+if (a_1 >= 19 and (a_2 == 'M',a_2 == 'W')) or  (b_1 >= 19 and (b_2 == 'M' or b_2 == 'W')):
     print('1')
 else:
     print('0')

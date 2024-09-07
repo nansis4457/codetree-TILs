d38c7a40de0c799ae77c2d5d8b1cad87ca17d998
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-07 23:09:44 +0900

    Title: 좀 더 어려운 수학 점수, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240907/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240907/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -2,9 +2,7 @@ a_1,a_2 = map(int,input().split())
 b_1,b_2 = map(int,input().split())
 
 
-if a_1 < b_1:
-    print('B')
-elif a_2 > b_2:
+if a_1 > b_1 or (a_1 == b_1 and a_2 > b_2):
     print('A')
 else:
-    print('A')
+    print('B')

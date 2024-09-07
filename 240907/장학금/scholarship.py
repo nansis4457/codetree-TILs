d84fad49740778324eeb1bf4abdcc55abeac436a
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-07 22:57:02 +0900

    Title: 장학금, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240907/장학금/scholarship.py
@@ -0,0 +1,11 @@
+n,m = map(int,input().split())
+
+if n >= 90:
+    elif m >= 95:
+        print('100000')
+    elif m >= 90:
+        print('50000')
+    else:
+        print('0')
+else:
+    print('0')

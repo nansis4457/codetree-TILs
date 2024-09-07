0d5ba7d027b040be9c60f1a045ecf45f07fc874d
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-07 23:38:44 +0900

    Title: 일 수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240907/일 수 구하기/number-of-days-in-month.py
+++ 240907/일 수 구하기/number-of-days-in-month.py
@@ -2,8 +2,8 @@ n = int(input())
 if n % 2 == 1:
     print('31')
 elif n != 2 and (n % 2 == 0):
-    elif n == '8'
-    print('31')
+    if n == '8'
+        print('31')
     else:
         print('30')
 else:

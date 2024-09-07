abd084d158e7a2da27f9c3441c047bfda7b2da30
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-07 23:54:58 +0900

    Title: 코로나 메뉴얼, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240907/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,43 @@
+a_1,a_2 = input().split()
+b_1,b_2 = input().split()
+c_1,c_2 = input().split()
+
+a_2,b_2,c_2 = int(a_2),int(b_2),int(c_2)
+A = 0 
+B = 0
+C = 0 
+if a_1 == 'Y':
+    if a_2 >= 37:
+        A = 'A'
+    else:
+        A = 'C'
+elif a_2 >= 37:
+        A = 'B'
+else:
+    A = 'D'
+
+if b_1 == 'Y':
+    if b_2 >= 37:
+        B = 'A'
+    else:
+        B = 'C'
+elif b_2 >= 37:
+    B = 'B'
+else:
+    B = 'D'
+
+if c_1 == 'Y':
+    if c_2 >= 37:
+        C = 'A'
+    else:
+        C = 'C'
+elif c_2 >= 37:
+    C = 'B'
+else:
+    C ='D'
+
+
+if A == 'A' and (B == 'A' or C == 'A'):
+    print('E')
+else:
+    print('N')

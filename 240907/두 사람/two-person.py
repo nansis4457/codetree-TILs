3ec15bdb32317cb91cfeb9f9c2f5ccd2accaca94
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-07 23:23:54 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240907/두 사람/two-person.py
+++ 240907/두 사람/two-person.py
@@ -3,7 +3,16 @@ b_1,b_2 = input().split()
 
 a_1,b_1 = int(a_1),int(b_1)
 
-if (a_1 >= 19 and (a_2 == 'M' or a_2 == 'W')) or  (b_1 >= 19 and (b_2 == 'M' or b_2 == 'W')):
+if (a_1 >= 19 and a_2 == 'M') or (b_1 >= 19 and b_2 == 'M'):
+    print('1')
+elif (a_1 < 19 or a_2 == 'W') or (b_1 < 19 or b_2 == 'W'):
+    print('0')
+else:
+    print(0)
+
+
+
+(a_2 == 'M' or a_2 == 'W')) or  (b_1 >= 19 and (b_2 == 'M' or b_2 == 'W')):
     print('1')
 else:
     print('0')

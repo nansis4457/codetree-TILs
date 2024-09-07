0430147fcbcf671fb84fd8d199b23e457d346d72
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-07 17:59:27 +0900

    Title: 특정 조건 두 정수 비교, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240907/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,13 @@
+a,b = map(int, input().split(' '))
+
+if a > b:
+    print('0', end =" ")
+elif a < b:
+    print('1', end =" ")
+
+
+
+if a == b: 
+    print('1', end =" ")
+else:
+    print('0', end =" ")

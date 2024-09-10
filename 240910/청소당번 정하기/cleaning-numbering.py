e51a9d91697644fed05b084ee9befcd64a28fbd6
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-10 22:25:07 +0900

    Title: 청소당번 정하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240910/청소당번 정하기/cleaning-numbering.py
+++ 240910/청소당번 정하기/cleaning-numbering.py
@@ -4,12 +4,14 @@ cnt3 = 0
 
 n = int(input())
 
-for i in range(1,n):
-    if i % 2 == 0 and i != 0 :
-        cnt1 += 1
-    if i % 3 == 0 and i != 0 :
-        cnt2 += 1
-    if i % 12 == 0 and i != 0 :
+for i in range(1,n+1):
+    if i % 12 == 0:
         cnt3 += 1
+    elif i % 3 == 0:
+        cnt2 += 1
+    elif i % 2 == 0:
+        cnt1 += 1
+   
+    
 
 print(cnt1, cnt2, cnt3)

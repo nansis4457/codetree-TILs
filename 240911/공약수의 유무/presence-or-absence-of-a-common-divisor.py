22c2e9fa76564924dfd11d269731cffa702cc133
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-11 13:31:11 +0900

    Title: 공약수의 유무, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240911/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split())
+n1 = 1920 ; n2 = 2880
+ 
+morphy = False
+for i in range(a,b+1):
+    if n1 % i == 0 or n2 % i == 0:
+        morphy = True
+
+if morphy == True:
+    print('1')
+else:
+    print("0")

d9f51284374e3f877ea76965e6653ad92bdf5fdb
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-11 13:06:42 +0900

    Title: 3n + 1 수열, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240911/3n + 1 수열/3n-plus-1-sequence.py
+++ 240911/3n + 1 수열/3n-plus-1-sequence.py
@@ -1,16 +1,10 @@
 n = int(input())
 cnt = 0
-while True:
+while n != 1:  # n이 1이 될 때까지 반복
     if n % 2 == 0:
         n = n // 2
-        cnt +=1
-    elif n % 2 == 1:
-        n = 3 * n + 1
-        cnt +=1
-    if n == 1:    
-        break
     else:
-        continue
-        
+        n = 3 * n + 1
+    cnt += 1  # 모든 반복에서 cnt 증가
 
 print(cnt)

da0bee692af74f63e90ec255e3ad2cc15b578ba8
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-11 12:50:26 +0900

    Title: 20대의 평균나이, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240911/20대의 평균나이/average-age-of-20.py
@@ -0,0 +1,15 @@
+c = 0
+cnt = 0
+while True:
+    n = int(input())
+    if n < 30:
+        c += n
+        cnt += 1
+        continue
+    elif n >= 30:
+        cnt += 1
+        break
+
+# 루프가 종료된 후 평균값 출력
+if cnt > 0:
+    print(f'{c / cnt:.2f}')

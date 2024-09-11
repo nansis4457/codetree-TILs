90de1d13c39569bb8fc797defcb167b3a127fe85
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-11 12:53:55 +0900

    Title: 20대의 평균나이, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240911/20대의 평균나이/average-age-of-20.py
+++ 240911/20대의 평균나이/average-age-of-20.py
@@ -8,9 +8,4 @@ while True:
         continue
     else:
         break
-
-# cnt가 0이 아닌 경우에만 평균을 출력
-if cnt > 0:
-    print(f'{c / cnt:.2f}')
-else:
-    print("입력된 숫자가 없습니다.")
+        print(f'{c / cnt:.2f}')

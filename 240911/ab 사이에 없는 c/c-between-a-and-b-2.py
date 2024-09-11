6aa5c806de6f913b38c7880ec93dc013b66874c6
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-11 13:42:09 +0900

    Title: ab 사이에 없는 c, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240911/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240911/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -1,12 +1,12 @@
 a, b, c = map(int, input().split())
 
-morphy = True
+morphy = True  # Assume all numbers are divisible initially
 for i in range(a, b + 1):
     if i % c != 0:
         morphy = False
-        break  # 한 번이라도 나누어 떨어지지 않으면 반복 종료
+        break  # Break the loop as soon as a number is not divisible by c
 
-if morphy == False:
-    print("YES")
+if morphy:
+    print("NO")  # All numbers in the range are divisible by c
 else:
-    print("NO")
+    print("YES")

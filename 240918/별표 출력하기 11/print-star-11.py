56d23b54b47ef067258816f13e2a91d0e6227e49
nansis4457 <127852445+nansis4457@users.noreply.github.com>
2024-09-18 16:11:34 +0900

    Title: 별표 출력하기 11, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240918/별표 출력하기 11/print-star-11.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(3*n-2): 
+    for j in range(3*n-2):
+        if i == 0 or i % 2 == 0 or j == 0 or j % 2 == 0 :
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
